1e07e944379529c7707e0e68ecf170d138ecad59
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-11-16 14:19:42 +0100

    Initial commit as a solution for the Java quest "Stream 02: Map" and some other snippets.

--- src/main/java/wildcodeschool/quests/Java_Streams/Map/Shield.java
+++ src/main/java/wildcodeschool/quests/Java_Streams/Map/Shield.java
@@ -18,17 +18,18 @@ public class Shield {
         // TODO 1 : map names to agents list
         // Variante 1
         // Inspiriert bei Armans-Lösung
-        /*List<Agent> agents = names.stream()
-                     .map(name -> name.split(" "))                                        // erzeugt einen Array vom Typ String mit zwei Einträgen
+
+        List<Agent> agents = names.stream()
+                     .map(name -> name.split(" "))                                  // erzeugt einen Array vom Typ String mit zwei Einträgen
                      .map(splitString -> new Agent(splitString[0], splitString[1]))       // erzeugt ein neues Agent-Objekt mit den Werten aus dem String-Array
                      .collect(Collectors.toList());
 
-        */
+
 
         // Variante 2
         // Dies war meine Idee zu "3. Use a Function in which you can call the methodsplit on the string to extract the first and last name:
         // use these to create an instance of an Agent and return it"
-        List<Agent> agents = names.stream()
+        List<Agent> agents2 = names.stream()
                         .map(name -> {
                             Agent agentX = new Agent();
                             agentX.setFirstName(name.split(" ")[0]);
@@ -38,6 +39,7 @@ public class Shield {
                 .collect(Collectors.toList());
 
         showAgents(agents);
+        showAgents(agents2);
     }
 
     // don't touch anything here !
